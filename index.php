7a7d171d5fa151b6a3a796249c6e027cb9c89ab5
Claudio <claudio.v.lima2@gmail.com>
2021-09-24 19:59:36 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+    include './Artigo.php';
+    require './config.php'; 
+
+    $artigo = new Artigo($mysql);
+    $artigos = $artigo->exibeArtigos();
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container bg-secondary p-3">
+        <h1>Artigos do Blog</h1>
+
+        <?php foreach($artigos as $artigo): ?>
+
+            <div class="artigo bg-light p-3 mb-2">
+                <a href=" <?php echo $artigo['titulo']  ?> "> 
+                    <h2"> <?php echo $artigo['titulo'] ?> </h2> 
+                </a>
+                <p> <?php echo $artigo['conteudo'] ?> </p>
+            </div>
+
+        <?php endforeach; ?>
+
+       
+    </div>
+</body>
+</html>
